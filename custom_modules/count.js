895eba414eba88cfd702b0c6fabe3af0c67ea684
LukeNK <lukenguyenkhanh@gmail.com>
2022-06-14 13:33:14 -0700

    Counting for multiple choices I love coding in school

--- custom_modules/count.js
+++ custom_modules/count.js
@@ -1,6 +1,12 @@
 let data = require('../data/data.json');
 let ballots = data.ballots;
-if (data.methodId == 4) {
+if (data.method == 2) {
+    let result = {};
+    for (const ballot of ballots)
+        if (result[ballot] == undefined) result[ballot] = 0;
+        else result[ballot]++;
+    console.table(result);
+} else if (data.methodId == 4) {
     let trueC = 0, falseC = 0;
     for (const ballot of ballots) {
         // Secure test to make sure no way the ballot was checked wrongly
